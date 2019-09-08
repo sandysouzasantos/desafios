8ba8e756389679d2423fb73b35397e838a8ccf5d
Sandy Santos <sandysouzasantos@gmail.com>
2019-09-08 15:58:30 -0300

    Improved crawler results apresentation

--- crawlers/crawler.js
+++ crawlers/crawler.js
@@ -2,16 +2,14 @@ const fs = require('fs');
 
 const request = require('superagent');
 const cheerio = require('cheerio');
-const url = require('url-parse');
 const htmlparser2 = require('htmlparser2');
 
 const redditUrl = 'https://old.reddit.com';
 const arguments = process.argv[2];
 
+fs.writeFileSync('reddit.txt', 'New in reddit \n');
 if (arguments) {
     const wantedSubreddits = arguments.split(';');
-
-    fs.writeFileSync('reddit.txt', 'New in reddit \n');
 
     wantedSubreddits.forEach((subreddit) => {
         const subredditUrl = `${redditUrl}/r/${subreddit.toString()}/`;
@@ -19,7 +17,7 @@ if (arguments) {
         searchPosts(subredditUrl, subreddit);
     });
 } else {
-    searchPosts(redditUrl, 'init');
+    searchPosts(redditUrl + '/r/all/', 'all');
 }
 
 async function searchPosts(url, subreddit) {
@@ -33,19 +31,22 @@ async function searchPosts(url, subreddit) {
         $('div#siteTable > div.thing').each(function (index) {
             const pontuation = $(this).attr('data-score');
             const title = $(this).find('p.title > a.title').text().trim();
-            // TODO resolver bug no threadLink qd ele for relativo ao site do reddit (comentários)
             let threadLink = $(this).find('p.title > a.title').attr('href');
             const commentsLink = $(this).find('ul.flat-list.buttons > li.first > a.comments').attr('href');
 
-            if(commentsLink.indexOf(threadLink) !== -1) {
+            if (commentsLink.indexOf(threadLink) !== -1) {
                 threadLink = commentsLink;
             }
 
             if (pontuation >= 5000) {
-                fs.appendFileSync('reddit.txt', subreddit + '\n' + title + '\n' + pontuation + '\n' + threadLink + '\n' + commentsLink + '\n');
+                fs.appendFileSync('reddit.txt', '/r/' + subreddit + '\n' +
+                    'Title: ' + title + '\n' +
+                    'Score: ' + pontuation + '\n' +
+                    'Thread: ' + threadLink + '\n' +
+                    'Comments: ' + commentsLink + '\n' + '\n');
             }
-        })
-
+        });
+        console.log('Look at reddit.txt');
     } catch (error) {
         console.log("Error: " + error);
     }
