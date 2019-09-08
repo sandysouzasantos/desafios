4a080da1fe564b7ee1de9b9abc68b0ccb0e9df46
Sandy Santos <sandysouzasantos@gmail.com>
2019-09-08 19:47:48 -0300

    Cleaned code

--- strings/index.js
+++ strings/index.js
@@ -1,42 +1,60 @@
-function justify(line, limit) {
-    return line;
-}
-
 function styleMyString(limit, myString) {
-    const charactersArray = myString.split(/[\s\n]/);
-
+    myString = myString.trim();
+    const wordsArray = myString.split(/[\s\n\t]/);
     let result = '';
-
-    let line = charactersArray[0];
-
-    for (let i = 1; i < charactersArray.length; i += 1) {
-        if ((line + ' ' + charactersArray[i]).length <= limit) {
-            line = line + ' ' + charactersArray[i];
+    let line = wordsArray[0];
+    for (let i = 1; i < wordsArray.length; i += 1) {
+        if ((line + ' ' + wordsArray[i]).length <= limit) {
+            if (wordsArray[i].length === 0) {
+                result += line + '\n';
+                line = '';
+                continue;
+            }
+            line = (line !== '' ? line + ' ' : '') + wordsArray[i];
         } else {
-            result += justify(line, limit) + '\n';
-            line = charactersArray[i];
+            result += line + '\n';
+            line = wordsArray[i];
         }
     }
-
-    result += justify(line, limit) + '\n';
+    result += line + '\n';
 
     let lineBreakPositions = [];
-
     let position = myString.indexOf('\n\n');
-    while(position !== -1) {
+    while (position !== -1) {
         lineBreakPositions.push(position);
-        position = myString.indexOf('\n\n', position+1);
+        position = myString.indexOf('\n\n', position + 1);
     }
 
     const characters = result.split('');
-    for(let i = 0; i < lineBreakPositions.length; i++){
+    for (let i = 0; i < lineBreakPositions.length; i++) {
         characters.splice(lineBreakPositions[i], 0, '\n');
     }
-
     result = characters.join('');
-    console.log(result);
+
+    const lines = result.split("\n");
+    let justifiedResult = '';
+    for (let i = 0; i < lines.length; i++) {
+        justifiedResult += justify(lines[i], limit) + '\n';
+    }
+    return justifiedResult;
 }
 
-styleMyString(40, "In the beginning God created the heavens and the earth. Now the earth was formless and empty, darkness was over the surface of the deep, and the Spirit of God was hovering over the waters.\n" +
-    "\n" +
-    "And God said, \"Let there be light,\" and there was light. God saw that the light was good, and he separated the light from the darkness. God called the light \"day,\" and the darkness he called \"night.\" And there was evening, and there was morning - the first day.");
+function justify(line, limit) {
+    if (line.length === 0) {
+        return line;
+    }
+    let missingSpaces = limit - line.length;
+    const words = line.split(' ');
+
+    while (missingSpaces > 0) {
+        for (let i = 0; i < words.length - 1; i++) {
+            words[i] += ' ';
+            missingSpaces--;
+            if (missingSpaces <= 0) {
+                break;
+            }
+        }
+    }
+
+    return words.join(' ');
+}
