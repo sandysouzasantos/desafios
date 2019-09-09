76541ecf9c50f37d679c0b6473c17a6cd2b8d982
Sandy Santos <sandysouzasantos@gmail.com>
2019-09-09 07:17:20 -0300

    Edited readme and added comments to index.js file

--- strings/README.md
+++ strings/README.md
@@ -1,17 +1,13 @@
 # Desafio 1: Strings
 
-Após ler o coding style do kernel Linux, você descobre a mágica que é
-ter linhas de código com no máximo 80 caracteres cada uma.
+## Como executar esse código
 
-Assim, você decide que de hoje em diante seus e-mails enviados também
-seguirão um padrão parecido e resolve desenvolver um plugin para te ajudar
-com isso. Contudo, seu plugin aceitará no máximo 40 caracteres por linha.
+Execute o arquivo html em seu browser (chrome ou firefox) e insira o número máximo de caracteres por linha e o texto desejado conforme os campos informados.
+Clique no botão 'Estilizar'.
 
-Implemente uma função que receba:
-1. um texto qualquer
-2. um limite de comprimento
+O resultado irá aparecer conforme os parâmetros informados e justificado.
 
-e seja capaz de gerar os outputs dos desafios abaixo.
+Detalhes de como o desafio foi resolvido se encontram no arquivo `index.js` na forma de comentários.
 
 ## Exemplo input
 
@@ -19,17 +15,4 @@ e seja capaz de gerar os outputs dos desafios abaixo.
 
 `And God said, "Let there be light," and there was light. God saw that the light was good, and he separated the light from the darkness. God called the light "day," and the darkness he called "night." And there was evening, and there was morning - the first day.`
 
-O texto deve ser parametrizável e se quiser, pode utilizar um texto de input de sua preferência.
 
-### Parte 1 (Básico) - limite 40 caracteres
-Você deve seguir o exemplo de output [deste arquivo](https://github.com/idwall/desafios/blob/master/strings/output_parte1.txt), onde basta o texto possuir, no máximo, 40 caracteres por linha. As palavras não podem ser quebradas no meio.
-
-### Parte 2 (Intermediário) - limite 40 caracteres
-O exemplo de output está [neste arquivo](https://github.com/idwall/desafios/blob/master/strings/output-parte2.txt), onde além de o arquivo possuir, no máximo, 40 caracteres por linha, o texto deve estar **justificado**.
-
-### Dicas
-- Existe um template para projetos em Java ;)
-
-### Extras
-
-- Parametrização da quantidade de caracteres por linha.
--- strings/index.js
+++ strings/index.js
@@ -1,23 +1,33 @@
 function styleMyString(limit, myString) {
     myString = myString.trim();
+    // Cria um array com todas as palavras da string dada.
     const wordsArray = myString.split(/[\s\n\t]/);
+    // Variável na qual as linhas já cortadas de acordo com o número de caracteres serão salvas.
     let result = '';
+    // Variável na qual as palavras serão adicionadas até o limite de caracteres, já começando na primeira palavra.
     let line = wordsArray[0];
+
+    // Para cada palavra do array, adicioná-la em 'line' até o limite de caracteres.
     for (let i = 1; i < wordsArray.length; i += 1) {
         if ((line + ' ' + wordsArray[i]).length <= limit) {
+            // Caso ocorra de alguma elemento do array ser vazio, finalizar a linha atual e começar uma nova.
             if (wordsArray[i].length === 0) {
                 result += line + '\n';
                 line = '';
                 continue;
             }
+            // Verifica se 'line' é vazia antes de adicionar uma nova palavra para evitar espaços no início da frase.
             line = (line !== '' ? line + ' ' : '') + wordsArray[i];
         } else {
+            // Tendo 'line' alcançado o máximo de caracteres sem quebrar nenhuma palavra, ela é salva e uma nova 'line' é iniciada.
             result += line + '\n';
             line = wordsArray[i];
         }
     }
+    // Como a última 'line' não entra na hipótese do 'else' e o loop já acabou porque não há mais palavras, salva-se ela no final.
     result += line + '\n';
 
+    // Aqui busca-se a posição a linha vazia na string original para readicioná-la à string.
     let lineBreakPositions = [];
     let position = myString.indexOf('\n\n');
     while (position !== -1) {
@@ -31,6 +41,7 @@ function styleMyString(limit, myString) {
     }
     result = characters.join('');
 
+    // Para justificar a nova string, crias-se um novo array com as frases e se chama a função 'justify' para cada uma delas.
     const lines = result.split("\n");
     let justifiedResult = '';
     for (let i = 0; i < lines.length; i++) {
@@ -43,9 +54,12 @@ function justify(line, limit) {
     if (line.length === 0) {
         return line;
     }
+
+    // Descobre quantos caracteres faltam para a frase aatingir o limite de caracteres.
     let missingSpaces = limit - line.length;
     const words = line.split(' ');
 
+    // Enquanto for possível acrescentar espaços à string, um espaço será adicionado ao final de cada palavra com exceção da última.
     while (missingSpaces > 0) {
         for (let i = 0; i < words.length - 1; i++) {
             words[i] += ' ';
